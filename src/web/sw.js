3388915f641b6126d2e37398aeec78ed50609848
Daniel Gomez <daniel.gomez@cqisense.com>
2026-03-02 10:36:57 +0100

    fix(pwa): Improve manifest compliance and icon pathing

--- src/web/sw.js
+++ src/web/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'flipper-kb-v1';
+const CACHE_NAME = 'flipper-kb-v2';
 const ASSETS = [
-  './',
-  './index.html',
-  './app.js',
-  './manifest.json',
-  './icon.svg'
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json',
+  '/icon.svg'
 ];
 
 self.addEventListener('install', (event) => {
